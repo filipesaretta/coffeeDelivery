15ab3f8525f326a835658698379565e5db7a0cd6
filipesaretta <filipessm124@gmail.com>
2022-08-19 09:17:46 -0300

    Build the desktop version

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,54 @@
+import { Header } from '../../components/Header'
+import { Features, HomeContainer, HomeDetails, Title } from './styles'
+
+import coffeMainImage from '../../assets/img/cofee-image-home.png'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <HomeContainer>
+        <HomeDetails>
+          <Title>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee DeVlivery você recebe seu café onde estiver, a
+              qualquer hora.
+            </p>
+          </Title>
+          <Features>
+            <p>
+              <span className="cart">
+                <ShoppingCart width={16} weight="fill" />
+              </span>
+              Compra simples e segura
+            </p>
+            <p>
+              <span className="timer">
+                <Timer width={16} weight="fill" />
+              </span>
+              Entrega rápida e rastreada
+            </p>
+            <p>
+              <span className="package">
+                <Package width={16} weight="fill" />
+              </span>
+              Embalagem mantém o café intacto
+            </p>
+            <p>
+              <span className="coffee">
+                <Coffee width={16} weight="fill" />
+              </span>
+              O café chega fresquinho até você
+            </p>
+          </Features>
+        </HomeDetails>
+        <img
+          src={coffeMainImage}
+          alt="A cup of coffee with some beans of coffee around it"
+        />
+      </HomeContainer>
+    </>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,75 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr max-content;
+  align-items: center;
+  gap: 3.5rem;
+  margin-top: 5.875rem;
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`
+
+export const HomeDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4.125rem;
+`
+export const Title = styled.div`
+  h1 {
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 900;
+    font-size: 2.5rem;
+    color: ${(props) => props.theme['base-title']};
+    line-height: 130%;
+  }
+
+  p {
+    font-size: 1.25rem;
+    color: ${(props) => props.theme['base-subtile']};
+    line-height: 130%;
+    margin-top: 1rem;
+  }
+`
+
+export const Features = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 2.5rem;
+  row-gap: 1.25rem;
+
+  p {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    color: ${(props) => props.theme['base-text']};
+  }
+
+  span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${(props) => props.theme.white};
+    min-height: 2rem;
+    min-width: 2rem;
+    border-radius: 100%;
+
+    &.cart {
+      background: ${(props) => props.theme['yellow-dark']};
+    }
+    &.timer {
+      background: ${(props) => props.theme.yellow};
+    }
+    &.package {
+      background: ${(props) => props.theme['base-text']};
+    }
+    &.coffee {
+      background: ${(props) => props.theme.purple};
+    }
+  }
+`
